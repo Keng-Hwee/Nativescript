7e242763e78431d6584428db188f09996ad61d85
Keng Hwee <kenghwee.t@gmail.com>
2020-01-01 13:46:23 +0800

    initialize app

---
+++ src/app/challenges/challenges.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'ns-challenges',
+  templateUrl: './challenges.component.html',
+  styleUrls: ['./challenges.component.css']
+})
+export class ChallengesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
